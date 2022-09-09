38647fef4e3e8fcdcda9a1c114b0d51c345d30ad
Sanskar Dixit <48208274+dixitsanskar@users.noreply.github.com>
2022-09-09 13:14:46 +0530

    Update total salary.c

--- total salary.c
+++ total salary.c
@@ -5,17 +5,17 @@ int main()
     int totalsalary,basic,allow;
     float hra,da,pf;
     char grade;
-    scanf("%d",&basic);
-    
-    scanf("%c",&grade);
+    scanf("%d%c",&basic,&grade);
     hra=0.2*basic;
     da=0.5*basic;
+    
     if(grade=='A')
     allow=1700;
     else if(grade=='B')
     allow=1500;
-    else
+    else 
     allow=1300;
+    
     pf=0.11*basic;
     totalsalary=(basic+hra+da+allow-pf);
     printf("%d",totalsalary);
